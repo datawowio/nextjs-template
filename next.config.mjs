cdad77ff1117bb33b7eed81560313caed14f1db7
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-08-19 14:00:48 +0700

    setup bundler analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from "@next/bundle-analyzer";
 
-export default nextConfig;
+// Analyzing your bundles
+// Run the following command to analyze your bundles
+// ANALYZE=true npm run build
+// Ref: https://nextjs.org/docs/app/building-your-application/optimizing/bundle-analyzer
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links
+    typedRoutes: true,
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
