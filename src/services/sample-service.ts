06834d0e629c0179e357e7817b79587f81980bef
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-08-28 10:24:57 +0700

    Remove POC files

--- src/services/sample-service.ts
+++
@@ -1,7 +0,0 @@
-import BaseService from "@/services/base-service";
-
-export default class SampleService extends BaseService {
-  getSamples(endpoint = "/api/v1/sample") {
-    return this.get(endpoint);
-  }
-}
