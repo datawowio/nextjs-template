ca62c19bb1b6ea5b450852903b28b3d0688b5615
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-08-28 09:21:33 +0700

    create default home page for next.js template (#11)
    * create default home page for next.js template
    
    * change favicon file location, delete middleware, and Thai translation

---
+++ src/screens/home/home-screen.tsx
@@ -0,0 +1,27 @@
+import { Box, Card, Stack, Typography } from "@mui/material";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+import logo from "@/assets/images/brands/logo.svg";
+import Button from "@/components/button";
+
+import { card, imageWrapper, root } from "./styles";
+
+export default function HomeScreen() {
+  const t = useTranslations("screens.home");
+
+  return (
+    <Box sx={root}>
+      <Card sx={card}>
+        <Stack gap={4}>
+          <Box sx={imageWrapper}>
+            <Image src={logo} alt={t("logoAlt")} fill />
+          </Box>
+          <Typography variant="h3">{t("heading")}</Typography>
+          <Typography>{t("description")}</Typography>
+          <Button variant="contained">{t("cta")}</Button>
+        </Stack>
+      </Card>
+    </Box>
+  );
+}
