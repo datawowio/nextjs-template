e4a8a09f7fd484963ab18eaf09f8438e5ca9c50d
Yutthana Siphuengchai <32380889+yutna@users.noreply.github.com>
2024-08-29 14:27:43 +0700

    Remove all component tests (#16)
    * Add test for home page
    
    * Remove all component test cases and simplify the locale import files.

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,18 +1,13 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
-export const locales = ["en", "th"];
+import getLocales from "@/locales";
+
+import type { Locale } from "@/types/locale";
+
+export const locales: Locale[] = ["en", "th"];
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
-
-  // Don't forget to import in alphabetical order.
-  return {
-    messages: {
-      ...(await import(`../locales/${locale}/common.json`)).default,
-      screens: {
-        ...(await import(`../locales/${locale}/screens/home.json`)).default,
-      },
-    },
-  };
+  return getLocales(locale as Locale);
 });
