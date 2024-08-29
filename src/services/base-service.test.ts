59fb848ce0f70f32f57d1def1d69954dd570e98c
Yutthana Siphuengchai <32380889+yutna@users.noreply.github.com>
2024-08-29 14:28:06 +0700

    POC best pattern for fetching data on server-side and client-side (#12)
    * Create base service class
    
    * Remove a comment
    
    * Add useSWR example
    
    * Add ENV example file
    
    * Update base service to use baseUrl from env
    
    * Remove POC files

---
+++ src/services/base-service.test.ts
@@ -0,0 +1,107 @@
+import BaseService from "./base-service";
+
+global.fetch = jest.fn();
+
+describe("BaseService", () => {
+  const baseURL = "http://localhost:3000";
+  const baseService = new BaseService();
+
+  beforeEach(() => {
+    (fetch as jest.Mock).mockClear();
+  });
+
+  it("calls fetch with correct URL and method for GET", async () => {
+    const endpoint = "/test-endpoint";
+    const mockResponse = { data: "test data" };
+
+    (fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockResponse),
+    });
+
+    const response = await baseService.get(endpoint);
+
+    expect(fetch).toHaveBeenCalledWith(baseURL + endpoint, { method: "GET" });
+    expect(response).toEqual(mockResponse);
+  });
+
+  it("calls fetch with correct URL, method, and body for POST", async () => {
+    const endpoint = "/test-endpoint";
+    const body = { key: "value" };
+    const mockResponse = { data: "test data" };
+
+    (fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockResponse),
+    });
+
+    const response = await baseService.post(endpoint, body);
+
+    expect(fetch).toHaveBeenCalledWith(baseURL + endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    expect(response).toEqual(mockResponse);
+  });
+
+  it("calls fetch with correct URL, method, and body for PUT", async () => {
+    const endpoint = "/test-endpoint";
+    const body = { key: "value" };
+    const mockResponse = { data: "test data" };
+
+    (fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockResponse),
+    });
+
+    const response = await baseService.put(endpoint, body);
+
+    expect(fetch).toHaveBeenCalledWith(baseURL + endpoint, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    expect(response).toEqual(mockResponse);
+  });
+
+  it("calls fetch with correct URL and method for DELETE", async () => {
+    const endpoint = "/test-endpoint";
+    const mockResponse = { data: "test data" };
+
+    (fetch as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockResponse),
+    });
+
+    const response = await baseService.delete(endpoint);
+
+    expect(fetch).toHaveBeenCalledWith(baseURL + endpoint, {
+      method: "DELETE",
+    });
+    expect(response).toEqual(mockResponse);
+  });
+
+  it("handles fetch errors", async () => {
+    const mockError = new Error("Network Error");
+    (fetch as jest.Mock).mockRejectedValue(mockError);
+
+    await expect(baseService.get("/test-endpoint")).rejects.toThrow(
+      "Network Error",
+    );
+    expect(fetch).toHaveBeenCalledWith(`${baseURL}/test-endpoint`, {
+      method: "GET",
+    });
+  });
+
+  it("handles non-OK responses", async () => {
+    const endpoint = "/test-endpoint";
+
+    (fetch as jest.Mock).mockResolvedValue({
+      ok: false,
+      status: 404,
+    });
+
+    await expect(baseService.get(endpoint)).rejects.toBe("Error: 404");
+  });
+});
---
+++ src/services/base-service.ts
@@ -0,0 +1,46 @@
+export default class BaseService {
+  private readonly baseURL: string;
+
+  constructor() {
+    this.baseURL = process.env.NEXT_PUBLIC_BASE_URL || "";
+  }
+
+  get(endpoint: string) {
+    return this.request(endpoint, {
+      method: "GET",
+    });
+  }
+
+  post(endpoint: string, body: any) {
+    return this.request(endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+  }
+
+  put(endpoint: string, body: any) {
+    return this.request(endpoint, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+  }
+
+  delete(endpoint: string) {
+    return this.request(endpoint, {
+      method: "DELETE",
+    });
+  }
+
+  private async request(endpoint: string, options: RequestInit) {
+    try {
+      const response = await fetch(this.baseURL + endpoint, options);
+      return await (response.ok
+        ? response.json()
+        : Promise.reject(`Error: ${response.status}`));
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
