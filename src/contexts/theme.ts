bd23e970c66eae2cf80081f74df9b40053bcf914
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-09 15:35:45 +0700

    fix imports

--- src/contexts/theme.ts
+++ src/contexts/theme.ts
@@ -1,8 +1,8 @@
 "use client";
 
+import { createTheme } from "@mui/material/styles";
 import { ibmPlexSansThai } from "@/config/fonts";
 import { MuiButton } from "@/styles/mui/button";
-import { createTheme } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
 const fontFamily = [
