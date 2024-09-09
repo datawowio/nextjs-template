0a8757be7b33935766c35e83bea37bee16d537ac
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-09-09 18:43:19 +0700

    Add tests for the card-color component

--- src/components/core/card-color/CardColor.tsx
+++ src/components/core/card-color/CardColor.tsx
@@ -1,17 +1,25 @@
-import { Box, hexToRgb, Stack } from "@mui/material";
+import { hexToRgb } from "@mui/material";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+
 import Typography from "@/components/core/typography";
+
 import { styles } from "./styles";
 
 import type { CardColorProps } from "./types";
 
 export default function CardColor({
-  colorStyles,
-  hexCode,
   colorName,
+  hexCode,
+
+  colorStyles,
 }: CardColorProps) {
   return (
-    <Stack sx={styles.container}>
-      <Box sx={{ ...colorStyles, borderRadius: "10px 10px 0 0" }} />
+    <Stack sx={styles.root}>
+      <Box
+        role="presentation"
+        sx={{ ...colorStyles, ...styles.colorPreview }}
+      />
       <Stack p={1} gap={1}>
         <Typography customVariant="mediumCaptionMD">{colorName}</Typography>
         <Typography customVariant="regularCaptionMD">{hexCode}</Typography>
