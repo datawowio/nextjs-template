d078214114e7be316521c164d7b3840cfd2c8973
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-18 14:58:39 +0700

    change checkbox component to match best practice

--- src/components/core/checkbox/Checkbox.tsx
+++ src/components/core/checkbox/Checkbox.tsx
@@ -1,11 +1,8 @@
+import FormControlLabel from "@mui/material/FormControlLabel";
 import MUICheckbox from "@mui/material/Checkbox";
-import Stack from "@mui/material/Stack";
 
-import Typography from "@/components/core/typography";
 import { colors } from "@/config/palette";
 import rem from "@/utils/rem";
-
-import { getCustomTextVariant } from "./helpers";
 import { BpCheckedIcon, BpIcon, BpIndeterminateIcon } from "./styles";
 
 import type { CheckboxProps } from "./types";
@@ -14,31 +11,33 @@ export default function Checkbox({
   disabled,
   label,
   size = "small",
-
+  labelProps,
   ...props
 }: CheckboxProps) {
   return (
-    <Stack direction="row" alignItems="center">
-      <MUICheckbox
-        {...props}
-        disabled={disabled}
-        checkedIcon={<BpCheckedIcon className={size} />}
-        icon={<BpIcon className={size} />}
-        indeterminateIcon={<BpIndeterminateIcon className={size} />}
-      />
-      {label && (
-        <Typography
-          customVariant={getCustomTextVariant(size)}
-          sx={{
+    <FormControlLabel
+      {...labelProps}
+      control={
+        <MUICheckbox
+          {...props}
+          disabled={disabled}
+          checkedIcon={<BpCheckedIcon className={size} />}
+          icon={<BpIcon className={size} />}
+          indeterminateIcon={<BpIndeterminateIcon className={size} />}
+        />
+      }
+      label={label}
+      className={size}
+      slotProps={{
+        typography: {
+          sx: {
             color: disabled
               ? colors.text.placeholder.disabled
               : colors.text.primary.message,
-            marginTop: rem(1),
-          }}
-        >
-          {label}
-        </Typography>
-      )}
-    </Stack>
+            marginTop: rem(2),
+          },
+        },
+      }}
+    />
   );
 }
--- src/components/core/checkbox/types.ts
+++ src/components/core/checkbox/types.ts
@@ -1,7 +1,10 @@
+import type { FormControlLabelProps } from "@mui/material";
 import type { CheckboxProps as MUICheckboxProps } from "@mui/material/Checkbox";
 
 export type CheckboxCustomSize = "small" | "medium" | "large";
 
 export interface CheckboxProps extends MUICheckboxProps {
+  labelProps: FormControlLabelProps;
   label?: string;
+  disableTypography?: boolean;
 }
