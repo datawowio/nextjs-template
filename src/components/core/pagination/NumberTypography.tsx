77968204749cf324eff595a4942d341a6b414d1a
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-18 11:07:35 +0700

    Create Table pagination component (#58)
    * create table pagination component
    
    * update margins and border radius
    
    * refactor NumberTypography component
    
    * fix reviewed comments

---
+++ src/components/core/pagination/NumberTypography.tsx
@@ -0,0 +1,19 @@
+import Typography from "@/components/core/typography";
+import { colors } from "@/config/palette";
+import type { ReactNode } from "react";
+
+export default function NumberTypography({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <Typography
+      component="span"
+      sx={{ color: colors.text.primary.header, mx: "8px" }}
+      customVariant="mediumParagraphLG"
+    >
+      {children}
+    </Typography>
+  );
+}
