cc9838d6295e8a2c3bd5636a9becf3cf54a932e0
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-19 16:41:05 +0700

    fix sidebar styling

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import Toolbar from "@mui/material/Toolbar";
+import { Toolbar } from "./styles";
 
 import type { SideBarProps } from "./types";
 
