79d649e69fc283c3137fc89252bf05973c8d5620
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-23 12:02:16 +0700

    change children to required type

--- src/components/core/header/types.ts
+++ src/components/core/header/types.ts
@@ -2,7 +2,7 @@ import type { SxProps } from "@mui/material";
 import type { ReactNode } from "react";
 
 export interface HeaderProps {
-  children: ReactNode;
+  children?: ReactNode;
   sx?: SxProps;
   text?: string;
 }
