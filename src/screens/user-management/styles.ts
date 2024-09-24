8b2151a9d252df90df7b2aefc5e19008712515d9
phoommdtw <146710174+phoommdtw@users.noreply.github.com>
2024-09-24 10:17:31 +0700

    remove unused styles

--- src/screens/user-management/styles.ts
+++ src/screens/user-management/styles.ts
@@ -1,5 +1,4 @@
 import { colors } from "@/config/palette";
-import rem from "@/utils/rem";
 import type { SxProps, Theme } from "@mui/material";
 
 export const card: SxProps<Theme> = {
@@ -11,21 +10,4 @@ export const flexWrapper: SxProps<Theme> = {
   display: "flex",
   gap: 2,
   alignItems: "center",
-};
-
-export const imageWrapper: SxProps<Theme> = {
-  position: "relative",
-  mx: "auto",
-  width: 400,
-  height: 160,
-};
-
-export const root: SxProps<Theme> = {
-  position: "relative",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  px: `0 ${rem(20)}`,
-  height: "100vh",
-  width: "100%",
 };
