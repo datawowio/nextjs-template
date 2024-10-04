da01dbc2bbc027373112e87fbf2b6ef7416c79a7
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-10-04 12:56:49 +0700

    Revise and improve the next-intl settings.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,10 +1,9 @@
 import type { Locale } from "@/types/locale";
 
-// Don't forget to import in alphabetical order.
 export default async function getLocales(locale: Locale = "en") {
   return {
     messages: {
-      ...(await import(`./${locale}/common.json`)).default,
+      ...(await import(`./${locale}/${locale}.json`)).default,
     },
   };
 }
