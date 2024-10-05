a50f6a2c37f4c54bb591bd8bd9a830c400bf784a
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-10-05 16:29:58 +0700

    Add locale translation for metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,32 @@
 import "server-only";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 
 import { routing } from "@/config/i18n";
 
 import LocaleProvider from "@/providers/locale-provider";
 import MuiThemeProvider from "@/providers/mui-theme-provider";
 
-import type { Metadata } from "next";
 import type { BaseLayoutProps } from "@/types/component";
+import type { Locale } from "@/types/locale";
 
-export const metadata: Metadata = {
-  description: "Datawow Next.js Boilerplate, built with App Router",
-  title: {
-    default: "Next.js Boilerplate",
-    template: "%s | Next.js Boilerplate",
-  },
-};
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}) {
+  const t = await getTranslations({
+    locale,
+    namespace: "common.metadata",
+  });
+
+  return {
+    description: t("description"),
+    title: {
+      default: t("title.default"),
+      template: t("title.template"),
+    },
+  };
+}
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
