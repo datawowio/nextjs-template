7a047890107cbb79b211ad79c2a04813308c2b55
Watcharit Tuntayakul <watcharit.new@gmail.com>
2024-12-09 14:41:43 +0700

    chore: move interface to type file

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,13 +1,8 @@
+import type { Env } from "@/types/env";
+
 const branch = process.env.NEXT_PUBLIC_BRANCH;
 
-interface Env {
-  BASE_STORYBOOK_URL: string;
-  NEXT_PUBLIC_BASE_URL: string;
-  NEXT_PUBLIC_BASE_STORYBOOK_URL: string;
-  IS_PROD: boolean;
-}
-
-const envObj: Env = {
+const ENV: Env = {
   BASE_STORYBOOK_URL: process.env.BASE_STORYBOOK_URL || "",
   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "",
   NEXT_PUBLIC_BASE_STORYBOOK_URL:
@@ -15,4 +10,4 @@ const envObj: Env = {
   IS_PROD: branch === "production",
 };
 
-export default envObj;
+export default ENV;
